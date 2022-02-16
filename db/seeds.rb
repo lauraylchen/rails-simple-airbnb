11d391741f444ebbeaf24e6a49f3595cfdcdaaf6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-16 15:16:21 -0500

    Added the frontend set up and gem faker to create data using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the db...'
+Flat.destroy_all
+
+puts 'Creating 4 flats...'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+3.times do
+  Flat.create!(
+    name: Faker::JapaneseMedia::SwordArtOnline.location,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(1..100),
+    number_of_guests: rand(1..100)
+  )
+end
+
+puts "There is #{Flat.count} flats"
