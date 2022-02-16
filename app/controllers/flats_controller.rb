2fa563985837dc8a6538954d8b330215dd577e21
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-16 16:27:06 -0500

    Completed actions new and create for user to add a flat and the view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,24 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests, :description)
+  end
 end
